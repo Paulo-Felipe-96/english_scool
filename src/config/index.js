d6b537806971d1ef4a84578d2921f9b13fd3a33b
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-06-29 00:03:21 -0300

    feat: add host to index

--- src/config/index.js
+++ src/config/index.js
@@ -4,3 +4,4 @@ export const port = process.env.PORT || 3000;
 export const dbUserName = process.env.MYSQL_USER;
 export const dbPassword = process.env.MYSQL_PASSWORD;
 export const database = process.env.MYSQL_DATABASE;
+export const host = process.env.HOST || "127.0.0.1";
