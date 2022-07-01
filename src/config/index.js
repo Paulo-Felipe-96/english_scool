97c37f0544bde292967212e119ff1e0d3d54f51b
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-01 17:41:41 -0300

    fix: modify db user and password

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
 export const port = process.env.PORT || 3000;
-export const dbUserName = process.env.MYSQL_USER;
-export const dbPassword = process.env.MYSQL_PASSWORD;
+export const dbUserName = process.env.MYSQL_ROOT;
+export const dbPassword = process.env.MYSQL_ROOT_PASSWORD;
 export const database = process.env.MYSQL_DATABASE;
 export const host = process.env.HOST || "127.0.0.1";
