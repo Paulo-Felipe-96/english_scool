d379e577e8d28e855cf1c3f875e81a91f2747735
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-01 17:38:27 -0300

    fix: modify mjs to be compatible with modules

--- src/config/database.mjs
+++ src/config/database.mjs
@@ -1,12 +1,11 @@
-import { database, dbUserName, dbPassword, host } from "./index.js";
+import * as dbConfig from "./index.js";
 
-module.exports = {
-  host: host, //path to the database
-  username: dbUserName, // database username
-  password: dbPassword, // database password
-  database: database, // database name
+const config = {
+  host: dbConfig.host, //path to the database
+  username: dbConfig.dbUserName, // database username
+  password: dbConfig.dbPassword, // database password
+  database: dbConfig.database, // database name
   dialect: "mysql", // type of database on we are working in
-  operatorsAliases: false, // disables a sequelize warning
   logging: false, // disables database logging
   define: {
     timestamps: true, // auto 'created_at' and 'updated_at'
@@ -14,3 +13,5 @@ module.exports = {
     underscoredAll: true, // underscores column names such as user_table or invoice_table eg
   },
 };
+
+export default config;
