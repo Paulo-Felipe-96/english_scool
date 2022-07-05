02e90b65829ec5a87c06bbeea3b310830efbbd9b
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-05 13:40:35 -0300

    refact: name changes

--- src/routes/peopleRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import PeopleController from "../controller/PeopleController.js";
-
-const routes = Router();
-const { listAll } = PeopleController;
-
-routes.get("/pessoas", listAll);
-
-export default routes;
