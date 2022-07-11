fc94b0ef95822a13c69131f57b11b2ddd49a985b
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-11 19:55:28 -0300

    feat: map dev and prod environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,25 @@
 const config = require("dotenv/config");
 
 const port = process.env.PORT || 3000;
-const dbUserName = process.env.MYSQL_ROOT;
-const dbPassword = process.env.MYSQL_ROOT_PASSWORD;
-const database = process.env.MYSQL_DATABASE;
+
+const dev_username = process.env.MYSQL_DEV_USER;
+const dev_password = process.env.MYSQL_DEV_PASSWORD;
+const prod_username = process.env.MYSQL_PROD_USER;
+const prod_password = process.env.MYSQL_PROD_PASSWORD;
+
+const dev_db = process.env.MYSQL_DEV;
+const prod_db = process.env.MYSQL_PROD;
 const host = process.env.HOST || "127.0.0.1";
+const environment = process.env.NODE_ENV;
 
 module.exports = {
   port,
-  dbUserName,
-  dbPassword,
-  database,
+  prod_username,
+  prod_password,
+  dev_username,
+  dev_password,
+  prod_db,
+  dev_db,
   host,
+  environment,
 };
