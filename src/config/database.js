c945de09b2d8b7946127a4b42c00bd069d062178
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-11 19:56:05 -0300

    fix: fix env variables to prod and dev

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,33 @@
-const { database, dbPassword, dbUserName, host } = require("../config");
+const {
+  prod_username,
+  prod_password,
+  dev_username,
+  dev_password,
+  prod_db,
+  dev_db,
+  host,
+} = require("../config");
 
 module.exports = {
   development: {
     host: host, //path to the database
-    username: dbUserName, // database username
-    password: dbPassword, // database password
-    database: database, // database name
+    username: dev_username, // database username
+    password: dev_password, // database password
+    database: dev_db, // database name
+    dialect: "mysql", // type of database on we are working in
+    logging: false, // disables database logging
+    define: {
+      freezeTableName: true, // remove plural naming as default
+      timestamps: true, // auto 'created_at' and 'updated_at'
+      underscored: true, // underscores table names such as user_database eg
+      underscoredAll: true, // underscores column names such as user_table or invoice_table eg
+    },
+  },
+  production: {
+    host: host, //path to the database
+    username: prod_username, // database username
+    password: prod_password, // database password
+    database: prod_db, // database name
     dialect: "mysql", // type of database on we are working in
     logging: false, // disables database logging
     define: {
