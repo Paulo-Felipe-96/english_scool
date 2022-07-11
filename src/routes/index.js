7172237c971864643ba48c1f9bc48ed678834f98
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-11 19:53:12 -0300

    feat: add status endpoint with environment info

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,17 @@
 const person = require("./personRoutes");
+const { environment } = require("../config");
 
 const routes = (app) => {
   app.get("/", (req, res) => {
-    res.status(200).send("Welcome!");
+    res.status(200).send("Seja bem-vinde!");
+  });
+
+  app.get("/status", (req, res) => {
+    try {
+      res.status(200).json({ status_code: 200, ambiente: environment });
+    } catch (error) {
+      res.status(500).json({ message: error });
+    }
   });
 
   app.use(person);
