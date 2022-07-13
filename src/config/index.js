74498eb7e29aba9a19090d1bec2e6bd0cbc191c7
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-13 20:51:19 -0300

    feat: config mapping for postgres

--- src/config/index.js
+++ src/config/index.js
@@ -3,13 +3,13 @@ const config = require("dotenv/config");
 const prod_port = process.env.PROD_PORT || 3001;
 const dev_port = process.env.DEV_PORT || 3000;
 
-const dev_username = process.env.MYSQL_DEV_USER;
-const dev_password = process.env.MYSQL_DEV_PASSWORD;
-const prod_username = process.env.MYSQL_PROD_USER;
-const prod_password = process.env.MYSQL_PROD_PASSWORD;
+const dev_username = process.env.POSTGRES_DEV_USER;
+const dev_password = process.env.POSTGRES_DEV_PASSWORD;
+const prod_username = process.env.POSTGRES_PROD_USER;
+const prod_password = process.env.POSTGRES_PROD_PASSWORD;
 
-const dev_db = process.env.MYSQL_DEV;
-const prod_db = process.env.MYSQL_PROD;
+const dev_db = process.env.DEV_DB;
+const prod_db = process.env.PROD_DB;
 const host = process.env.HOST || "127.0.0.1";
 const environment = process.env.NODE_ENV || "development";
 
