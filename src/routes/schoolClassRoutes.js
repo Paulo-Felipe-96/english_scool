3a7acad06fa53fbdd125168c34a2e9c8447b0f97
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-07-14 15:19:22 -0300

    fix: route index refer

--- src/routes/schoolClassRoutes.js
+++ src/routes/schoolClassRoutes.js
@@ -1,21 +1,20 @@
 const router = require("express").Router();
 const {
   findAllSchoolClasses,
-  findAllSchoolClassById,
-  findAllSchoolClassByLevelId,
-  findAllSchoolClassByTeacherId,
-  findAllSchoolClassByStartDate,
-  insertSchoolClass,
-  updateSchoolClassById,
+  // findAllSchoolClassById,
+  // findAllSchoolClassByLevelId,
+  // findAllSchoolClassByTeacherId,
+  // findAllSchoolClassByStartDate,
+  // insertSchoolClass,
+  // updateSchoolClassById,
 } = require("../controller/SchoolClassController");
 
 //data inicio / docente / nivel
 
-router
-  .get("/turmas", findAllSchoolClasses)
-  .get("/turmas/:id", findAllSchoolClassById)
-  .get("/turmas/nivel/:id_nivel", findAllSchoolClassByLevelId)
-  .get("/turmas/docente/:id_docente", findAllSchoolClassByTeacherId)
-  .get("/turmas/data/?data_inicio", findAllSchoolClassByStartDate)
-  .post("/turmas", insertSchoolClass)
-  .put("/turmas/:id", updateSchoolClassById);
+router.get("/turmas", findAllSchoolClasses);
+// .get("/turmas/:id", findAllSchoolClassById)
+// .get("/turmas/nivel/:id_nivel", findAllSchoolClassByLevelId)
+// .get("/turmas/docente/:id_docente", findAllSchoolClassByTeacherId)
+// .get("/turmas/data/?data_inicio", findAllSchoolClassByStartDate)
+// .post("/turmas", insertSchoolClass)
+// .put("/turmas/:id", updateSchoolClassById);
