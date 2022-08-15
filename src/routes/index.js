4efb50080d3ceef255c4fa7d835aa2a0fc182581
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-08-15 15:06:26 -0300

    refact: welcome message with postman collection

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,15 @@ const person = require("./personRoutes");
 const level = require("./levelRoutes");
 const schoolClass = require("./schoolClassRoutes");
 const { environment } = require("../config");
+const endpointsUrl =
+  "https://github.com/Paulo-Felipe-96/english_scool/blob/feat/nexstep-sequelize/English%20Scool.postman_collection.json";
 
 const routes = (app) => {
   app.get("/", (req, res) => {
-    res.status(200).send("Seja bem-vinde!");
+    res.status(200).json({
+      message: "Seja bem-vinde!",
+      info: `Veja os endpoints aqui: ${endpointsUrl}`,
+    });
   });
 
   app.get("/status", (req, res) => {
