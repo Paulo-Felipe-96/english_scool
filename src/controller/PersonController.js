24f9eb7aad8f850a4c37afde2d3f400e816b2dc5
Paulo-Felipe-96 <paulofelipe0896@gmail.com>
2022-08-15 18:56:43 -0300

    feat: add basic transaction to cancel enrolls

--- src/controller/PersonController.js
+++ src/controller/PersonController.js
@@ -315,27 +315,31 @@ class PersonController {
     const { id } = req.params;
 
     try {
-      const person = await db.Pessoas.scope("studantRole").update(
-        { ativo: false },
-        { where: { id } }
-      );
-
-      if (person[0] === 1) {
-        const enrollments = await db.Matriculas.update(
-          { status: "cancelado" },
-          { where: { id_estudante: id } }
+      db.sequelize.transaction(async (transaction) => {
+        const person = await db.Pessoas.scope("studantRole").update(
+          { ativo: false },
+          { where: { id } },
+          { transaction: transaction }
         );
 
-        res.status(200).json({
-          message: "sucesso",
-          id_estudante: id,
-          registros_afetados: enrollments,
-        });
-      } else {
-        res
-          .status(404)
-          .json({ message: "estudante não encontrado ou inativo" });
-      }
+        if (person[0] === 1) {
+          const enrollments = await db.Matriculas.update(
+            { status: "cancelado" },
+            { where: { id_estudante: id } },
+            { transaction: transaction }
+          );
+
+          res.status(200).json({
+            message: "sucesso",
+            id_estudante: id,
+            registros_afetados: enrollments,
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: "estudante não encontrado ou inativo" });
+        }
+      });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
